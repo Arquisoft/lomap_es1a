2aaeaabf863b26ce83c5bde7d93ce498aa299142
MarcosCaraduje <uo270285@uniovi.es>
2023-03-13 17:17:17 +0100

    Añade metodo post a api

--- restapi/api.ts
+++ restapi/api.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response, Router } from 'express';
 import {check} from 'express-validator';
+import LocationController from './location';
+import LocationModel from './location';
 
 const api:Router = express.Router()
 
@@ -8,9 +10,11 @@ interface User {
     email: string;
 }
 
+
 //This is not a restapi as it mantains state but it is here for
 //simplicity. A database should be used instead.
 let users: Array<User> = [];
+let locations: Array<LocationModel> = [];
 
 api.get(
     "/users/list",
@@ -32,5 +36,24 @@ api.post(
     return res.sendStatus(200);
   }
 );
+
+api.post(
+  "/location/add",
+  async (req: Request, res: Response): Promise<Response> => {
+    let id = req.body.id;
+    let longitude = req.body.longitude;
+    let latitude = req.body.latitude;
+    let category = req.body.category;
+
+    locations.push(new LocationModel (id, longitude, latitude, category));
+    return res.sendStatus(200);
+  }
+);
+api.get(
+  "location/list",
+  async (req:Request, res:Response): Promise<Response> => {
+    return res.status(200).send(users);
+  }
+)
 
 export default api;
