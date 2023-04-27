5197c5dff5daebb7718ee1250156854925456dc9
Andrés Ángel González Granda <uo68216@uniovi.es>
2023-04-28 01:53:37 +0200

    Se añade CORS a webapp. Se modifica fichero server.ts

--- webapp/server.ts
+++ webapp/server.ts
@@ -1,11 +1,18 @@
 import express,{Application} from 'express'; 
+import cors from "cors";
 //for using an import here we need to configure the tsconfig.json
 //setting the option module to commonjs
 
 var app: Application = express()
 const port: number = 3000;
 
-app.use(express.static('build'))
+
+app.use(cors({
+    origin: '*', //Admitimos todos los origenes 
+    credentials: true
+}));
+
+app.use(express.static('build'));
 
 app.listen(port, ():void => {
     console.log('Webapp started on port '+ port);
