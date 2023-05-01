58cb0839ba10c967b0acb60c7e669b53530eeb9d
uo264578 <uo264578@uniovi.es>
2023-05-01 18:16:23 +0300

    Update MapTest.test.tsx

--- webapp/src/test/MapTest.test.tsx
+++ webapp/src/test/MapTest.test.tsx
@@ -1,27 +1,25 @@
 import React from 'react';
 import Map from '../components/Map';
+import { render, fireEvent } from '@testing-library/react';
 import ReactDOM from 'react-dom';
 
 describe('Map component', () => {
-  const mockProps = {
-    lng: -73.935242,
-    lat: 40.73061,
-    zoom: 10,
-    mapWidth: '100%',
-    mapHeight: '500px',
-    mapTheme: 'light',
-  };
+  let container: HTMLDivElement
+
+  beforeEach(() => {
+    container = document.createElement('div');
+    document.body.appendChild(container);
+    ReactDOM.render(<Map lng={4.34878} lat={50.85045} zoom={10} mapWidth='100%' mapHeight='100%' mapTheme="light"/>,container)
+  })
+
+  afterEach(() =>{
+    document.body.removeChild(container);
+    container.remove();
+  }
+  )
 
   it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Map {...mockProps} />, div);
-    ReactDOM.unmountComponentAtNode(div);
+    const m = container.querySelector("#mapboxgl-canvas")
+    expect(m).toBeInTheDocument();
   });
-
-  it('sets up the map correctly', () => {
-    const map = new Map(mockProps);
-    expect(map.map).not.toBeNull();
-    expect(map.mapMarkers).toHaveLength(0);
-  });
-
 });
